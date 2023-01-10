0d2c1afa85329dd41519eaa48c3da1197ed84680
tree4553 <tree4553@naver.com>
2023-01-10 17:13:49 +0900

    Thread 동기화

--- src/thread/ThreadExample1.java
+++ src/thread/ThreadExample1.java
@@ -1,6 +1,6 @@
-package Thread;
+package thread;
 
-public class threadExample1 {
+public class ThreadExample1 {
     public static void main(String[] args) {
         Runnable task1 = new ThreadTask1();
         Thread thread1 = new Thread(task1);
