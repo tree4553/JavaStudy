4f52e61372639d06120bf8f9702ce7acc52ac319
tree4553 <tree4553@naver.com>
2023-03-14 16:25:14 +0900

    Baekjoon 10171, 25083

---
+++ src/Algorithm/Baekjoon/B_10171.java
@@ -0,0 +1,10 @@
+package Algorithm.Baekjoon;
+
+public class B_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
